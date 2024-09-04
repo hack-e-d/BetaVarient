ec2afa9ae8bc3b3b323f0c8992b26fa1a51d581d
vijay <Vijay.PV@expeditors.com>
2024-09-04 15:13:04 +0530

    Sum of two numbers without '+' or '-'

---
+++ src/main/java/vijay/projects/betavarient/LeetCode/SeventyFiveChallenge/IntegerSum.java
@@ -0,0 +1,19 @@
+package vijay.projects.betavarient.LeetCode.SeventyFiveChallenge;
+
+public class IntegerSum {
+
+    public int twoSum(int a, int b) {
+        int sum = 0, carry = 0;
+        do {
+            sum = a ^ b;
+            carry = a & b;
+            a = sum;
+            b = carry << 1;
+        } while (carry != 0);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new IntegerSum().twoSum(6,2));
+    }
+}
